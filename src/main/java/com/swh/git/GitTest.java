698737b44a6372d1a0ad2f0d4334a10ad8b5c6c5
无鸡汁弹 <110047395+xiaguruiwen@users.noreply.github.com>
2022-07-27 02:16:31 +0800

    pull commit

--- src/main/java/com/swh/git/GitTest.java
+++ src/main/java/com/swh/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("niubiGit2");
         System.out.println("niubiGit3");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
