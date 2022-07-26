5ab7d29d7b2234d08a09eb14b1ab12a1e687fa72
chinaman <chinaman@qq.com>
2022-07-27 01:40:25 +0800

    push commit

--- src/main/java/com/swh/git/GitTest.java
+++ src/main/java/com/swh/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("niubiGit");
         System.out.println("niubiGit2");
         System.out.println("niubiGit3");
+        System.out.println("push test!");
     }
 }
