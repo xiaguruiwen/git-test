162dc332b58270de6ad32715c599d52138852447
chinaman <chinaman@qq.com>
2022-07-27 00:56:44 +0800

    hot-fix commit1

--- src/main/java/com/swh/git/GitTest.java
+++ src/main/java/com/swh/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("niubiGit");
         System.out.println("niubiGit2");
+        System.out.println("niubiGit3");
     }
 }
